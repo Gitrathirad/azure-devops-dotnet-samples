55e10eef72b89d8eb5dad67309e3144072a144fa
Gitrathirad <37181342+Gitrathirad@users.noreply.github.com>
2019-07-24 22:35:45 +0530

    Update BuildTriggerReport.cs

--- ClientLibrary/Samples/Build/BuildTriggerReport.cs
+++ ClientLibrary/Samples/Build/BuildTriggerReport.cs
@@ -1,52 +1,29 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
-using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
-using Microsoft.VisualStudio.Services.Common;
-using Microsoft.VisualStudio.Services.WebApi;
-
-namespace ConsoleApp
+public static async void GetProjects()
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length == 3)
-            {
-                Uri orgUrl = new Uri(args[0]);         // Organization URL, for example: https://dev.azure.com/fabrikam               
-                String personalAccessToken = args[1];  // See https://docs.microsoft.com/azure/devops/integrate/get-started/authentication/pats
-                int workItemId = int.Parse(args[2]);   // ID of a work item, for example: 12
+	try
+	{
+		var personalaccesstoken = "PAT_FROM_WEBSITE";
 
-                // Create a connection
-                VssConnection connection = new VssConnection(orgUrl, new VssBasicCredential(string.Empty, personalAccessToken));
+		using (HttpClient client = new HttpClient())
+		{
+			client.DefaultRequestHeaders.Accept.Add(
+				new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
-                // Show details a work item
-                ShowBuildUserDetails(connection, projectname).Wait();
-            }
-            else
-            {
-                Console.WriteLine("Usage: ConsoleApp {orgUrl} {personalAccessToken} {projectname}");
-            }
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+				Convert.ToBase64String(
+					System.Text.ASCIIEncoding.ASCII.GetBytes(
+						string.Format("{0}:{1}", "", personalaccesstoken))));
 
-        }
-
-        static private async Task ShowBuildUserDetails(VssConnection connection, string projectname)
-        {
-            // Get Code for ShowBuildUserDetails*********
-            
-
-            try
-            {
-                //Code for ShowBuildUserDetails
-            }
-            catch (AggregateException aex)
-            {
-                VssServiceException vssex = aex.InnerException as VssServiceException;
-                if (vssex != null)
-                {
-                    Console.WriteLine(vssex.Message);
-                }
-            }
-        }
-    }
-}
+			using (HttpResponseMessage response = await client.GetAsync(
+						"https://dev.azure.com/{organization}/_apis/projects"))
+			{
+				response.EnsureSuccessStatusCode();
+				string responseBody = await response.Content.ReadAsStringAsync();
+				Console.WriteLine(responseBody);
+			}
+		}
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine(ex.ToString());
+	}
